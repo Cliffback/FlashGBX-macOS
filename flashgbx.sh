588c48fffbd8e2056563477443d42fb6e39d594d
Cliffback <51080320+Cliffback@users.noreply.github.com>
2024-01-30 08:04:24 +0000

    handling already created dmg

--- flashgbx.sh
+++ flashgbx.sh
@@ -10,7 +10,10 @@ function updateFlashGBX() {
       installed_version="0"
     fi
 
-    latest_version=$(curl -s https://api.github.com/repos/lesserkuma/FlashGBX/releases/latest | grep '"tag_name":' | awk -F\" '{print $4}') || { echo "Error fetching latest version information"; exit 1; }
+    latest_version=$(curl -s https://api.github.com/repos/lesserkuma/FlashGBX/releases/latest | grep '"tag_name":' | awk -F\" '{print $4}') || {
+      echo "Error fetching latest version information"
+      exit 1
+    }
 
     if [ "$latest_version" = "$installed_version" ]; then
       echo "FlashGBX is already up-to-date (version $installed_version)."
@@ -23,24 +26,32 @@ function updateFlashGBX() {
     echo -n "Do you want to update? (y/n): "
 
     read -r user_input
-    if [[ ! $user_input =~ ^[Yy]$ ]]
-    then
+    if [[ ! $user_input =~ ^[Yy]$ ]]; then
       echo "Update cancelled."
       return 0
     fi
 
     if [ ! -d "$repo_dir" ]; then
       mkdir -p "$repo_dir"
-      git clone https://github.com/lesserkuma/FlashGBX.git "$repo_dir" || { echo "Error cloning repository"; exit 1; }
+      git clone https://github.com/lesserkuma/FlashGBX.git "$repo_dir" || {
+        echo "Error cloning repository"
+        exit 1
+      }
       cd "$repo_dir"
     else
       cd "$repo_dir"
-      git fetch || { echo "Error fetching repository updates"; exit 1; }
+      git fetch || {
+        echo "Error fetching repository updates"
+        exit 1
+      }
     fi
-    
+
     # Checkout latest release
-    git checkout tags/$latest_version > /dev/null 2>&1 || { echo "Error checking out tag $latest_version"; exit 1; }
-    
+    git checkout tags/"$latest_version" >/dev/null 2>&1 || {
+      echo "Error checking out tag $latest_version"
+      exit 1
+    }
+
     #update_spec
     printf "# -*- mode: python ; coding: utf-8 -*-\n\n\
 a = Analysis(\n\
@@ -96,8 +107,8 @@ app = BUNDLE(\n\
     icon='FlashGBX/res/icon.ico',\n\
     bundle_identifier='com.lesserkuma.FlashGBX',\n\
     info_plist=info_plist,\n\
-)" > FlashGBX.spec
-    
+)" >FlashGBX.spec
+
     yes | pyinstaller FlashGBX.spec
     #plutil -replace CFBundleShortVersionString -string "$latest_version" dist/FlashGBX.app/Contents/Info.plist
     rsync -a --delete dist/FlashGBX.app/ /Applications/FlashGBX.app/
@@ -114,30 +125,41 @@ function createFlashGBXImage {
 
     repo_dir="$HOME/.tmp/FlashGBX"
 
-    latest_version=$(curl -s https://api.github.com/repos/lesserkuma/FlashGBX/releases/latest | grep '"tag_name":' | awk -F\" '{print $4}') || { echo "Error fetching latest version information"; exit 1; }
+    latest_version=$(curl -s https://api.github.com/repos/lesserkuma/FlashGBX/releases/latest | grep '"tag_name":' | awk -F\" '{print $4}') || {
+      echo "Error fetching latest version information"
+      exit 1
+    }
 
     # Prompt for user confirmation
     echo -n "Do you want to create an image of FlashGBX v$latest_version? (y/n): "
 
     read -r user_input
-    if [[ ! $user_input =~ ^[Yy]$ ]]
-    then
+    if [[ ! $user_input =~ ^[Yy]$ ]]; then
       echo "Creation cancelled."
       return 0
     fi
 
     if [ ! -d "$repo_dir" ]; then
       mkdir -p "$repo_dir"
-      git clone https://github.com/lesserkuma/FlashGBX.git "$repo_dir" || { echo "Error cloning repository"; exit 1; }
+      git clone https://github.com/lesserkuma/FlashGBX.git "$repo_dir" || {
+        echo "Error cloning repository"
+        exit 1
+      }
       cd "$repo_dir"
     else
       cd "$repo_dir"
-      git fetch || { echo "Error fetching repository updates"; exit 1; }
+      git fetch || {
+        echo "Error fetching repository updates"
+        exit 1
+      }
     fi
-    
+
     # Checkout latest release
-    git checkout tags/$latest_version > /dev/null 2>&1 || { echo "Error checking out tag $latest_version"; exit 1; }
-    
+    git checkout tags/"$latest_version" >/dev/null 2>&1 || {
+      echo "Error checking out tag $latest_version"
+      exit 1
+    }
+
     #update_spec
     printf "# -*- mode: python ; coding: utf-8 -*-\n\n\
 a = Analysis(\n\
@@ -193,23 +215,31 @@ app = BUNDLE(\n\
     icon='FlashGBX/res/icon.ico',\n\
     bundle_identifier='com.lesserkuma.FlashGBX',\n\
     info_plist=info_plist,\n\
-)" > FlashGBX.spec
-    
+)" >FlashGBX.spec
+
     yes | pyinstaller FlashGBX.spec
     mkdir -p dist/dmg
     cp -r "dist/FlashGBX.app" dist/dmg
+
+    # Check if the DMG file already exists and remove it if it does
+    dmg_path="dist/FlashGBX.dmg"
+    if [ -f "$dmg_path" ]; then
+      echo "Removing existing DMG file..."
+      rm "$dmg_path"
+    fi
+
     create-dmg \
-  --volname "FlashGBX" \
-  --volicon "FlashGBX/res/icon.ico" \
-  --window-pos 200 120 \
-  --window-size 600 300 \
-  --icon-size 100 \
-  --icon "FlashGBX.app" 175 120 \
-  --hide-extension "FlashGBX.app" \
-  --app-drop-link 425 120 \
-  "dist/FlashGBX.dmg" \
-  "dist/dmg/"
-  
+      --volname "FlashGBX" \
+      --volicon "FlashGBX/res/icon.ico" \
+      --window-pos 200 120 \
+      --window-size 600 300 \
+      --icon-size 100 \
+      --icon "FlashGBX.app" 175 120 \
+      --hide-extension "FlashGBX.app" \
+      --app-drop-link 425 120 \
+      "dist/FlashGBX.dmg" \
+      "dist/dmg/"
+
     echo "Successfully created image of FlashGBX v$latest_version."
 
   )
